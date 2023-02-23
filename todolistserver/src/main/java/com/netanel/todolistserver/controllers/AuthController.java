97052f7b405c6be79db94950120252e5aebbe3b8
Netanel Elikamel <netanelikamel@gmail.com>
2023-02-23 17:30:49 +0200

    Improving AuthController.

--- todolistserver/src/main/java/com/netanel/todolistserver/controllers/AuthController.java
+++ todolistserver/src/main/java/com/netanel/todolistserver/controllers/AuthController.java
@@ -4,6 +4,7 @@ import com.netanel.todolistserver.entities.User;
 import com.netanel.todolistserver.repos.UserRepository;
 import com.netanel.todolistserver.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
@@ -28,28 +29,28 @@ public class AuthController {
     }
 
     @GetMapping
-    public String home(Principal principal) {
-        return "Hello " + principal.getName() + "!";
+    public ResponseEntity<String> home(Principal principal) {
+        return ResponseEntity.ok("Hello " + principal.getName() + "!");
     }
 
     @PostMapping("/token")
-    public String token(Authentication authentication) {
+    public ResponseEntity<String> token(Authentication authentication) {
         String token = tokenService.generateAuthToken(authentication);
-        return token;
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/register")
-    public String register(@RequestBody User credentials) {
+    public ResponseEntity<String> register(@RequestBody User credentials) {
         if (credentials.getUsername().length() < 2 || credentials.getPassword().length() < 5)
-            return "Invalid Username or Password. Username min length: 2, Password min length: 5.";
+            return ResponseEntity.badRequest().body("Invalid Username or Password. Username min length: 2, Password min length: 5.");
 
         if (!isValidEmail(credentials.getEmail())) {
-            return "Invalid email format.";
+            return ResponseEntity.badRequest().body("Invalid email format.");
         }
 
         User user = userRepository.findByEmail(credentials.getEmail());
         if(user != null)
-            return "A user with the given email is already exists.";
+            return ResponseEntity.badRequest().body("A user with the given email already exists.");
 
         user = new User();
         user.setUsername(credentials.getUsername());
@@ -57,28 +58,30 @@ public class AuthController {
         user.setPassword(passwordEncoder.encode(credentials.getPassword()));
         user = userRepository.save(user);
 
-        return tokenService.generateToken(user.getId());
+        String token = tokenService.generateToken(user.getId());
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody User credentials) {
+    public ResponseEntity<String> login(@RequestBody User credentials) {
         if (credentials.getPassword().length() < 5)
-            return "Invalid Username or Password.";
+            return ResponseEntity.badRequest().body("Invalid Username or Password.");
 
         if (!isValidEmail(credentials.getEmail())) {
-            return "Invalid email format.";
+            return ResponseEntity.badRequest().body("Invalid email format.");
         }
 
         User user = userRepository.findByEmail(credentials.getEmail());
         if (user == null) {
-            return "A user with the given email is not exists.";
+            return ResponseEntity.badRequest().body("A user with the given email does not exist.");
         }
 
         if (!passwordEncoder.matches(credentials.getPassword(), user.getPassword())) {
-            return "Invalid email or password.";
+            return ResponseEntity.badRequest().body("Invalid email or password.");
         }
 
-        return tokenService.generateToken(user.getId());
+        String token = tokenService.generateToken(user.getId());
+        return ResponseEntity.ok(token);
     }
 
     public boolean isValidEmail(String email) {
